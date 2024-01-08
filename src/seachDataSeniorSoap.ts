e0dc0b631a0ec3cda3e93be3f71175bd42ba2e41
isaque-cwb <isaque.lourenco2@gmail.com>
2024-01-08 09:50:06 -0300

    chore: refactored function

--- src/seachDataSeniorSoap.ts
+++ src/seachDataSeniorSoap.ts
@@ -1,31 +1,30 @@
 import * as soap from 'soap'
+import { ApiError } from './helpers/api-erros'
 
 export const seachDataSeniorSoap = () => {
   const url = process.env.URL as string
-
-  soap.createClient(url, (err, client) => {
-    if (err) {
-      //precisa tratar o erro
-      console.log(err)
-    } else {
-      client.WS034FUN(
-        {
-          user: process.env.USER,
-          password: process.env.PASSWORD,
-          encryption: 0,
-          parameters: {
-            adatini: ''
-          }
-        },
-        (err: any, result: any) => {
-          if (err) {
-            //precisa tratar o erro
-            return console.log(err)
-          } else {
-            return console.log(result.result)
-          }
-        }
-      )
+  const body = {
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    encryption: 0,
+    parameters: {
+      adatini: ''
     }
-  })
+  }
+
+  soap
+    .createClientAsync(url)
+    .then(async client => {
+      await new Promise((resolve, reject) => {
+        return client.WS034FUN(body, (err: any, result: any) => {
+          if (err) {
+            reject(console.log(err))
+          }
+          resolve(console.log(result.result))
+        })
+      })
+    })
+    .catch(err => {
+      throw new ApiError('Erro de conexão da URL', 404)
+    })
 }
