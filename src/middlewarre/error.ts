a7a71f3e84b93c506ab842f4d2f298ebf4dd6482
isaque-cwb <isaque.lourenco2@gmail.com>
2024-01-08 09:51:56 -0300

    feat: errors customized created

---
+++ src/middlewarre/error.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from 'express'
+import { ApiError } from '../helpers/api-erros'
+
+export const errorMiddlware = (
+  error: Error & Partial<ApiError>,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const statusCode = error.statusCode ?? 500
+  const message = error.statusCode ? error.message : 'Internal Server Error'
+
+  return res.status(statusCode).json({
+    statusCode: statusCode,
+    message: message
+  })
+}
